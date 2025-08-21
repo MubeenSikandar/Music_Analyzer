ee902dda2a4e484652e20be33a80e5b1f1dde64c
Muhammad Mubeen Sikandar <68024800+MubeenSikandar@users.noreply.github.com>
2025-08-21 23:57:47 +0500

    Music_Analyzer

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use raylib::prelude::*;
+
+fn main() {
+    let (mut rl, thread) = raylib::init()
+        .size(640, 480)
+        .title("Music Analyzer")
+        .build();
+    let audio_device = RaylibAudio::init_audio_device().expect("Failed to init audio");
+    let ready: bool = audio_device.is_audio_device_ready();
+
+    let music = audio_device
+        .new_music("resources/Vivo.mp3")
+        .expect("Failed to Load Music");
+
+    music.play_stream();
+
+    println!("Audio device ready? {}", ready);
+
+    while !rl.window_should_close() {
+        music.update_stream();
+
+        if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
+            if music.is_stream_playing() {
+                music.pause_stream();
+            } else {
+                music.play_stream();
+            }
+        }
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::BLACK);
+        d.draw_circle_gradient(320, 200, 220.5, Color::DARKRED, Color::BLACK);
+        d.draw_text("Press SPACE to toggle music!", 170, 200, 20, Color::WHITE);
+    }
+}
